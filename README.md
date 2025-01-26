f60696fbb49924278777f0c2085d1e55c5b17ef2
Pernell Louis-Pierre <djtriet@gmail.com>
2025-01-26 12:25:44 -0500

    Initial commit with project setup, will change structure after

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # stock-trading-system
 IFT 401 Project
+
+# Working on the functionality of everything and customizing the repository!
---
+++ test.py
@@ -0,0 +1,8 @@
+x = 10
+y = 9
+
+def addNumbers(firstNum, secondNum) -> int:
+    result = firstNum + secondNum ** 2
+    return result
+
+print(f"test to see if this program provides output: {addNumbers(x,y)}")
